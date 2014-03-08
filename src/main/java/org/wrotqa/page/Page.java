b9f2340ad1e8110a7f54cc03709526604ebe65f8
Mateusz Koncikowski <mateusz.koncikowski@fastwhitecat.com>
2014-03-08 07:29:32 +0100

    Wrotqa #2 Initial commit

---
+++ src/main/java/org/wrotqa/page/Page.java
@@ -0,0 +1,10 @@
+package org.wrotqa.page;
+
+/**
+ * User: Mateusz Koncikowski
+ * Date: 08/03/14
+ * Time: 07:23
+ */
+
+public class Page {
+}
---
+++ src/test/java/org/wrotqa/PageObjectPatternTests.java
@@ -0,0 +1,17 @@
+package org.wrotqa;
+
+import org.junit.Test;
+
+/**
+ * User: Mateusz Koncikowski
+ * Date: 08/03/14
+ * Time: 07:21
+ */
+
+public class PageObjectPatternTests {
+
+    @Test
+    public void test() {
+
+    }
+}
